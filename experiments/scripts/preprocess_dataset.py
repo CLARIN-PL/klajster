361cc4bd1f9fee7e9c25d2e2b5d621db2971d3d0
djaniak <denis.janiak7@gmail.com>
2023-11-15 18:37:54 +0100

    init

---
+++ experiments/scripts/preprocess_dataset.py
@@ -0,0 +1,32 @@
+from typing import Type
+
+import typer
+from datasets import DatasetDict
+from embeddings.pipeline.preprocessing_pipeline import PreprocessingPipeline
+
+from klajster.paths import get_dataset_config_path
+from klajster.setup import disable_hf_datasets_caching
+from klajster.utils import get_module_from_str, prepare_output_path, read_yaml
+
+app = typer.Typer()
+
+
+def run(
+    ds: str = typer.Option(..., help="Dataset name."),
+    cfg_type: str = typer.Option(..., help="Config type."),
+    is_hps: bool = typer.Option(False, help="Is HPS Dataset"),
+) -> None:
+    cfg_type = cfg_type if not is_hps else f"{cfg_type}_hps"
+    disable_hf_datasets_caching()
+    cfg = read_yaml(get_dataset_config_path(ds))["datasets"][cfg_type]
+
+    pipeline_kwargs = cfg["pipeline_args"]
+    pipeline_kwargs["persist_path"] = prepare_output_path(cfg["output"])
+    pipeline_cls: Type[PreprocessingPipeline[str, DatasetDict, DatasetDict]] = get_module_from_str(
+        cfg["pipeline_cls"]
+    )
+    pipeline = pipeline_cls(**pipeline_kwargs)
+    pipeline.run()
+
+
+typer.run(run)
