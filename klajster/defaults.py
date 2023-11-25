1645ab99d271331aeb46cccb57c612e890301e9e
djaniak <denis.janiak7@gmail.com>
2023-11-25 21:29:22 +0100

    Multi-GPU fixes
    - update and cleanup project structure, improve run scripts
    - add dedicated configs for polemo2 and update project structure
    - rename DVC stage
    - update configs and run script
    - fix multi-gpu runs
    - refactor (black)

--- klajster/defaults.py
+++
@@ -1,9 +0,0 @@
-from typing import Dict, Union
-
-
-def get_model_checkpoint_kwargs() -> Dict[str, Union[str, None, bool]]:
-    return {
-        "filename": "last",
-        "monitor": None,
-        "save_last": False,
-    }
