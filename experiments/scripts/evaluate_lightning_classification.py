53897e70364ba952e39c0c544d70d09431c0e132
djaniak <denis.janiak7@gmail.com>
2023-11-29 22:17:03 +0100

    add writing results to specific eval file; fix num_nodes arg

--- experiments/scripts/evaluate_lightning_classification.py
+++ experiments/scripts/evaluate_lightning_classification.py
@@ -1,3 +1,5 @@
+import json
+import time
 from copy import deepcopy
 from pathlib import Path
 from typing import Dict, Union
@@ -29,6 +31,15 @@ def get_model_checkpoint_kwargs() -> Dict[str, Union[str, None, bool]]:
     }
 
 
+def write_evaluation_results(output_path: Union[str, Path], elapsed_time: float):
+    with open(output_path / "evaluation.json", "r") as f:
+        evaluation_results = json.load(f)
+    del evaluation_results["data"]
+    evaluation_results = {"total_time_in_sec": elapsed_time, "metrics": evaluation_results}
+    with open(output_path / "evaluation_lumi.json", "w") as f:
+        json.dump(evaluation_results, f, indent=4)
+
+
 def run(
     embedding_path: str = typer.Option("...", help="Embedding path."),
     ds: str = typer.Option("...", help="Dataset name."),
@@ -37,8 +48,8 @@ def run(
         help="Path to pipeline parameters config. If `None` the script will try to read the parameters configuration from a default HPS location",
     ),
     output_path: Path = typer.Option("...", help="Output path for model results."),
-    num_nodes: str = typer.Option("...", help="Number of compute nodes."),
-    devices: str = typer.Option("...", help="Number of GPUs."),
+    num_nodes: int = typer.Option("...", help="Number of compute nodes."),
+    devices: int = typer.Option("...", help="Number of GPUs."),
     accelerator: str = typer.Option("...", help="Accelerator type."),
     retrains: int = typer.Option(1, help="Number of model retrains."),
     wandb_entity: str = typer.Option("graph-ml-lab-wust", help="WandB entity."),
@@ -65,9 +76,9 @@ def run(
         tracking_project_name=tracking_project_name, wandb_entity=wandb_entity
     )
     cfg["model_checkpoint_kwargs"] = get_model_checkpoint_kwargs()
-    cfg["num_nodes"] = num_nodes
     cfg["devices"] = devices
     cfg["accelerator"] = accelerator
+    cfg["config"].task_train_kwargs["num_nodes"] = num_nodes
 
     for run_id in tqdm(range(retrains), desc="Run"):
         run_cfg = deepcopy(cfg)
@@ -78,7 +89,10 @@ def run(
         run_name = get_task_run_name(
             dataset=dataset_name, embedding_path=embedding_path, run_id=run_id
         )
+        start = time.time()
         pipeline.run(run_name=run_name)
+        end = time.time()
+        write_evaluation_results(output_path=run_output_path, elapsed_time=(end - start))
 
 
 if __name__ == "__main__":
